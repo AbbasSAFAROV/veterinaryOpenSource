6bda0189f0bf4970cc9c3aeb74d512ada67db437
FIRST_NAME LAST_NAME <abbasabdullaziz@gmail.com>
2022-01-01 00:58:01 +0300

    OwnerDto and PetDto added

---
+++ src/main/java/com/ozguryazilim/veterinary/model/OwnerDto.java
@@ -0,0 +1,11 @@
+package com.ozguryazilim.veterinary.model;
+
+public class OwnerDto {
+
+    private String nameSurname;
+    private String contact;
+    private String phoneNumber;
+    private String email;
+    private String password;
+
+}
---
+++ src/main/java/com/ozguryazilim/veterinary/model/PetDto.java
@@ -0,0 +1,11 @@
+package com.ozguryazilim.veterinary.model;
+
+public class PetDto {
+
+    private String name;
+    private String description;
+    private Integer age;
+    private String type;
+    private String genus;
+
+}
