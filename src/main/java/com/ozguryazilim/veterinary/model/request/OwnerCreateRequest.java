4a512e61f1c37898bf27df7749623f7ec3aea60a
FIRST_NAME LAST_NAME <abbasabdullaziz@gmail.com>
2022-01-01 03:31:04 +0300

    OwnerRequest and PetRequest added

---
+++ src/main/java/com/ozguryazilim/veterinary/model/request/OwnerCreateRequest.java
@@ -0,0 +1,11 @@
+package com.ozguryazilim.veterinary.model.request;
+
+public class OwnerCreateRequest {
+
+    private String nameSurname;
+    private String contact;
+    private String phoneNumber;
+    private String email;
+    private String password;
+
+}
---
+++ src/main/java/com/ozguryazilim/veterinary/model/request/PetCreateRequest.java
@@ -0,0 +1,11 @@
+package com.ozguryazilim.veterinary.model.request;
+
+public class PetCreateRequest {
+
+    private String name;
+    private String description;
+    private Integer age;
+    private String type;
+    private String genus;
+
+}
