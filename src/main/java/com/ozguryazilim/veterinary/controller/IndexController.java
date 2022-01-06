f8596e13b9f383930c8812dccf594c309a943bae
FIRST_NAME LAST_NAME <abbasabdullaziz@gmail.com>
2022-01-06 17:45:05 +0300

    index page added

--- src/main/java/com/ozguryazilim/veterinary/controller/IndexController.java
+++ src/main/java/com/ozguryazilim/veterinary/controller/IndexController.java
@@ -1,14 +1,26 @@
 package com.ozguryazilim.veterinary.controller;
 
+import com.ozguryazilim.veterinary.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
+    private final UserService ownerService;
+    private final ModelMapper modelMapper;
+
+    public IndexController(UserService ownerService, ModelMapper modelMapper) {
+        this.ownerService = ownerService;
+        this.modelMapper = modelMapper;
+    }
+
     @GetMapping("/")
-    public String getIndexPage(){
-        return "index";
+    public String getIndexPage(Model model){
+        model.addAttribute("owners",ownerService.getAllOwner());
+        return "owner";
     }
 
 }
--- src/main/java/com/ozguryazilim/veterinary/controller/MainController.java
+++ src/main/java/com/ozguryazilim/veterinary/controller/MainController.java
@@ -10,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/owner")
 public class MainController {
 
     private final UserService ownerService;
@@ -21,13 +20,13 @@ public class MainController {
         this.modelMapper = modelMapper;
     }
 
-    @GetMapping()
+    @GetMapping("/owner")
     public String getAllOwners(Model model){
         model.addAttribute("owners",ownerService.getAllOwner());
         return "owner";
     }
 
-    @GetMapping("/login")
+    @GetMapping("/owner/login")
     public String getLoginPage(){
         return "login";
     }
@@ -37,39 +36,46 @@ public class MainController {
         return new Owner();
     }
 
-    @GetMapping("/save")
+    @GetMapping("/owner/save")
     public String getRegisterSavePage(Model model){
         return "register";
     }
 
-    @PostMapping("/save")
+    @PostMapping("/owner/save")
     public String saveOwnerAccount(@ModelAttribute("user") Owner owner){
         ownerService.save(owner);
-        String redirect = "redirect:/owner/register?success";
-        return "redirect:/owner/register?success";
+        //String redirect = "redirect:/owner/register?success";
+        return "redirect:/owner?register=success";
     }
 
-    @GetMapping("/delete/{id}")
+    @GetMapping("/owner/delete/{id}")
     public String deleteOwnerById(@PathVariable("id") Long id){
         ownerService.deleteOwnerById(id);
         return "redirect:/owner";
     }
 
-    @GetMapping("/update/{id}")
+    @GetMapping("/owner/update/{id}")
     public String getUpdatePage(@PathVariable("id") Long id, Model model){
         Owner user = modelMapper.map(ownerService.getOwnerById(id),Owner.class);
         model.addAttribute("user",user);
         return "update";
     }
 
-    @PostMapping("/update")
-    public String updateOwner(@ModelAttribute("user") Owner owner){
-        ownerService.updateOwner(owner);
+    @PostMapping("/owner/edit/{id}")
+    public String updateOwner(@PathVariable("id") Long id,@ModelAttribute("user") Owner owner){
+        ownerService.updateOwnerById(owner,id);
+        //ownerService.updateOwner(owner);
         return "redirect:/owner";
     }
 
+    @GetMapping("/owner/search")
+    public String searchOwner(@RequestParam("search") String search,Model model){
+        Owner owner = modelMapper.map(ownerService.findByName(search),Owner.class);
+        model.addAttribute("owner",owner);
+        return "detail";
+    }
 
-    @GetMapping("/activate/admin/{id}")
+    @GetMapping("/owner/activate/admin/{id}")
     public String getActivateAdmin(@PathVariable("id") Long id){
         ownerService.activateAdmin(id);
         return "redirect:/owner";
